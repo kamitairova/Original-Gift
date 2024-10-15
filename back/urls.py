b80ddcac8ef5d995665431053ccfae5c74463c9a
theHalf <ilak04634@gmail.com>
2024-10-15 20:47:16 +0600

    first try

--- back/urls.py
+++ back/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+
+from django.conf.urls.static import static
+from django.conf import settings
+
+
 from main.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index , name='index'),
+    path('', index, name='index'),
 ]
+
+
--- main/models.py
+++ main/models.py
@@ -2,8 +2,17 @@ from django.db import models
 
 
 class Cards(models.Model):
-    image = models.ImageField(upload_to='static/images/')
+    image = models.ImageField(upload_to='CardIco')
     title = models.CharField(max_length=32, unique=True)
-    list = models.TextField()
-    text = models.TextField()
+    list = models.CharField(max_length=15)
+    text = models.TextField(max_length=25)
     cost = models.TextField(max_length=25)
+
+    def __str__(self):
+        return f"карточка: {self.title}|{self.text}: {self.cost}"
+
+
+
+
+
+
