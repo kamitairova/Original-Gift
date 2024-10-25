3c6495b4ce5329301b8062067fae6b608a90fed0
kamitairova <kamilla070703@gmail.com>
2024-10-25 13:16:19 +0600

    123456

--- back/settings.py
+++ back/settings.py
@@ -77,22 +77,17 @@ WSGI_APPLICATION = 'back.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'original_gift',
-#         'USER': 'jisus',
-#         'PASSWORD': '21585128',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'original_gift',
+        'USER': 'jisus',
+        'PASSWORD': '21585128',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
